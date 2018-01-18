01db7cf04e4078cd8bb8743527bdc2ff947cfd25
tw7613781 <tw7613781@gmail.com>
2018-01-19 02:24:29 +0900

    整体完成，但是参数传到JS里面显示不出来

--- crimemap.py
+++ crimemap.py
@@ -3,6 +3,7 @@ from dbhelper import DBHelper
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
 
 app = Flask(__name__)
 DB = DBHelper()
@@ -10,11 +11,13 @@ DB = DBHelper()
 @app.route('/')
 def home():
     try:
-        data = DB.get_all_inputs()
+        crimes = DB.get_all_crimes()
+        crimes = json.dumps(crimes)
     except Exception as e:
         print e
-        data = None
-    return render_template('home.html', data=data)
+        crimes = None
+    print crimes
+    return render_template('home.html', crimes=crimes)
 
 @app.route('/add', methods = ['POST'])
 def add():
@@ -31,6 +34,17 @@ def clear():
         DB.clear_all()
     except Exception as e:
         print e
+    return home()
+
+@app.route('/submitcrime', methods=['POST'])
+def submitcrime():
+    category = request.form.get('category')
+    date = request.form.get('date')
+    print date
+    latitude = float(request.form.get('latitude'))
+    longitude = float(request.form.get('longitude'))
+    description = request.form.get('description')
+    DB.add_crime(category, date, latitude, longitude, description)
     return home()
 
 if __name__ == '__main__':
