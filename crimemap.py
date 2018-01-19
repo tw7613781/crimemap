5de0183bdcd9a3639b6f077073de90cb1fde31eb
tw7613781 <tw7613781@gmail.com>
2018-01-20 00:45:41 +0900

    add user input validation

--- crimemap.py
+++ crimemap.py
@@ -5,20 +5,25 @@ from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
+import string
+import datetime
+import dateparser
 import json
 
 app = Flask(__name__)
 DB = DBHelper()
 
+categories = ['mugging', 'break-in']
+
 @app.route('/')
-def home():
+def home(error_message = None):
     try:
         crimes = DB.get_all_crimes()
         crimes = json.dumps(crimes)
     except Exception as e:
         print e
         crimes = None
-    return render_template('home.html', crimes=crimes)
+    return render_template('home.html', crimes=crimes, categories=categories, error_message = error_message)
 
 @app.route('/add', methods = ['POST'])
 def add():
@@ -40,13 +45,31 @@ def clear():
 @app.route('/submitcrime', methods=['POST'])
 def submitcrime():
     category = request.form.get('category')
-    date = request.form.get('date')
-    print date
-    latitude = float(request.form.get('latitude'))
-    longitude = float(request.form.get('longitude'))
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get('date'))
+    if not date:
+        return home('Invalid date. Please use yyyy-mm-dd format')
+    try:
+        latitude = float(request.form.get('latitude'))
+        longitude = float(request.form.get('longitude'))
+    except ValueError:
+        return home()
     description = request.form.get('description')
+    description = sanitize_string(description)
     DB.add_crime(category, date, latitude, longitude, description)
-    return redirect(url_for('home'))
+    return home()
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+def sanitize_string(userinput):
+    whitelist = string.letters + string.digits + '!@#$%^&*() '
+    return filter(lambda x:x in whitelist, userinput)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
